669a38286d91e089dab684892d263a6ce4ab04c8
VishwakSaran <vishwak@codewalnut.com>
2026-05-26 21:08:21 +0530

    Fix:android login page CSS issue

--- miraitu-app/src/app/layout.tsx
+++ miraitu-app/src/app/layout.tsx
@@ -139,6 +139,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: "#2c5926",
 };
 
